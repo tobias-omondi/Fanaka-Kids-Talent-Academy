83988568bbd5bf9803f423ef79ccda4aa5b46c5e
tobias <tobias.omondi@student.moringaschool.com>
2025-02-21 16:04:20 +0300

    email field

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -88,6 +88,7 @@ class Ranking(models.Model):
     
 class Message (models.Model):
     parent_name = models.CharField(max_length=30 )
+    email = models.EmailField(max_length=200 , null= False , blank= False)
     message = models.TextField(null=False , blank=False)
     phone_number = models.CharField( max_length=15,
      validators=[RegexValidator(r'^\+?1?\d{9,15}$', 'Enter a valid phone number.')]
