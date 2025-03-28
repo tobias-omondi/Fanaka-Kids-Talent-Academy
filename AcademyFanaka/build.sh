761aaeb7ee51dba5bb2e3cbc54c92a66c7a2db10
tobias <tobias.omondi@student.moringaschool.com>
2025-03-28 10:58:48 +0300

    ready for deployment

--- AcademyFanaka/build.sh
+++ AcademyFanaka/build.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Install Python dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
-
-# Collect static files
 python manage.py collectstatic --noinput
-
-# Apply database migrations
 python manage.py migrate
